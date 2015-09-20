90806fb2a17bb89ecc62f83c940207b9756b5afa
DaiFukumoto <wo.horn.dai@gmail.com>
2015-09-20 17:38:37 +0900

    Herokuで動かせるようにしてみる．

--- boot.ex
+++ boot.ex
@@ -1,1 +1,6 @@
 GakiBot.start_link(Application.get_env(:GakiBot, :api_key), [])
+
+case System.get_env("API_KEY") do
+  nil -> Application.get_env(:GakiBot, :api_key)
+  s -> s
+end |> GakiBot.start_link([])
